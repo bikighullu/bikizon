1c8281a4a8c824235840cffbf023912a65ce7b6b
biki ghullu <biki.ghulu@gmail.com>
2022-11-01 13:29:33 +1030

    app created

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCPvhfWGOH6bb55r9oPHntasCbtAwfvF2s",
+  authDomain: "bikizon-9a218.firebaseapp.com",
+  projectId: "bikizon-9a218",
+  storageBucket: "bikizon-9a218.appspot.com",
+  messagingSenderId: "843499654318",
+  appId: "1:843499654318:web:db872d919558ffe99df13f",
+};
